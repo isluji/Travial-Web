3f16068a997158e65295c36c49a3778fe061135d
Ismael Luque Jiménez <isluji@gmail.com>
2016-10-27 10:54:18 +0200

    Actualizado proyecto de Django

---
+++ travial_web/travial_web_app/apps.py
@@ -0,0 +1,7 @@
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class TravialWebAppConfig(AppConfig):
+    name = 'travial_web_app'
---
+++ travial_web/travial_web_app/models.py
@@ -0,0 +1,19 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from datetime import date
+
+# Create your models here.
+class Player(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    city = models.CharField(max_length=50)
+    birthdate = models.DateField(auto_now=False, auto_now_add=False)
+
+    def is_adult(self):
+        today = date.today()
+        age = today.year - self.birthdate.year - ((today.month, today.day) < (self.birthdate.month, self.birthdate.day))
+
+        return (age >= 18)
+
+    def __str__(self):
+        return self.nombre
