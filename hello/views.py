bbe26011b809814fe41bdf133b986268f39de649
Ismael Luque Jiménez <isluji@gmail.com>
2016-11-09 13:12:32 +0100

    Config vars demo

--- hello/views.py
+++ hello/views.py
@@ -4,14 +4,20 @@ from django.http import HttpResponse
 from .models import Greeting
 
 import requests
+import os
+
+# def index(request):
+#     # return HttpResponse('Hello from Python!')
+#     return render(request, 'index.html')
+#
+# def index(request):
+#     r = requests.get('http://httpbin.org/status/418')
+#     print r.text
+#     return HttpResponse('<pre>' + r.text + '</pre>')
 
 def index(request):
-    r = requests.get('http://httpbin.org/status/418')
-    print r.text
-
-    # return HttpResponse('Hello from Python!')
-    # return render(request, 'index.html')
-    return HttpResponse('<pre>' + r.text + '</pre>')
+    times = int(os.environ.get('TIMES',3))
+    return HttpResponse('Hello! ' * times)
 
 def db(request):
     greeting = Greeting()
