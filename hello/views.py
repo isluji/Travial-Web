93cebba8cce4572052a2524c30e042d18369046d
Ismael Luque Jiménez <isluji@gmail.com>
2016-11-09 12:24:20 +0100

    Demo

--- hello/views.py
+++ hello/views.py
@@ -3,18 +3,20 @@ from django.http import HttpResponse
 
 from .models import Greeting
 
-# Create your views here.
-def index(request):
-    # return HttpResponse('Hello from Python!')
-    return render(request, 'index.html')
+import requests
 
+def index(request):
+    r = requests.get('http://httpbin.org/status/418')
+    print r.text
+
+    # return HttpResponse('Hello from Python!')
+    # return render(request, 'index.html')
+    return HttpResponse('<pre>' + r.text + '</pre>')
 
 def db(request):
-
     greeting = Greeting()
     greeting.save()
 
     greetings = Greeting.objects.all()
 
     return render(request, 'db.html', {'greetings': greetings})
-
