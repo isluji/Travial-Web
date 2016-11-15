c61a86ec73362b8213fe30516b62ac496a47a99e
Ismael Luque Jiménez <isluji@gmail.com>
2016-11-15 02:28:33 +0100

    closes #12

--- travial_web/urls.py
+++ travial_web/urls.py
@@ -27,6 +27,5 @@ import travial_web_app.views
 
 urlpatterns = [
     url(r'^$', travial_web_app.views.index, name='index'),
-    url(r'^db', travial_web_app.views.db, name='db'),
     url(r'^admin/', include(admin.site.urls)),
 ]
